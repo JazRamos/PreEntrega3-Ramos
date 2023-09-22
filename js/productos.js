59ebae0e214d4cf1859446f6c9514ebd1047bbc8
Jaz Ramos <jazminaramos4@hotmail.com>
2023-09-22 18:20:27 -0300

    2

--- js/productos.js
+++ js/productos.js
@@ -2,25 +2,29 @@ const productos = [{
     id: 1,
     foto: "https://cdn.pixabay.com/photo/2018/04/05/08/47/agate-3292211_1280.jpg",
     nombre: "Ágata",
-    precio: 14999
+    descripcion: "Transmite energía positiva, vitalidad aumentando el autoestima. Logros materiales.",
+    precio: $1000
 },
 {
     id: 2,
     foto: "https://cdn.pixabay.com/photo/2018/04/17/17/28/amethyst-3328161_1280.jpg",
     nombre: "Amatista",
-    precio: 26300
+    descripcion: "Piedra de la transmutación. Calma la mente. Facilita el sueño.",
+    precio: $1000
 },
 {
     id: 3,
     foto: "https://cdn.pixabay.com/photo/2021/03/01/04/38/amazonite-6058741_1280.jpg",
     nombre: "Amazonita",
-    precio: 1200
+    descripcion: "Facilita la autoexpresión. Alivia la pena emocional. Disipa miedo e inseguridad.",
+    precio: $1000
 },
 {
     id: 4,
     foto: "https://cdn.pixabay.com/photo/2018/03/05/19/49/citrine-3201605_1280.jpg",
     nombre: "Citrino",
-    precio: 5900
+    descripcion: "Autoestima. Poder personal. Planes y objetivos. Confianza en uno mismo.",
+    precio: $1000
 }
 
 ];
