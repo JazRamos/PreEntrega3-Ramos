a30095cd8ee1a5c1626115ad11b3564d28e7c20a
Jaz Ramos <jazminaramos4@hotmail.com>
2023-09-22 11:46:40 -0300

    prod

---
+++ js/productos.js
@@ -0,0 +1,26 @@
+const productos = [{
+    id: 1,
+    foto: "https://cdn.pixabay.com/photo/2018/04/05/08/47/agate-3292211_1280.jpg",
+    nombre: "Ágata",
+    precio: 14999
+},
+{
+    id: 2,
+    foto: "https://cdn.pixabay.com/photo/2018/04/17/17/28/amethyst-3328161_1280.jpg",
+    nombre: "Amatista",
+    precio: 26300
+},
+{
+    id: 3,
+    foto: "https://cdn.pixabay.com/photo/2021/03/01/04/38/amazonite-6058741_1280.jpg",
+    nombre: "Amazonita",
+    precio: 1200
+},
+{
+    id: 4,
+    foto: "https://cdn.pixabay.com/photo/2018/03/05/19/49/citrine-3201605_1280.jpg",
+    nombre: "Citrino",
+    precio: 5900
+}
+
+];
