5b2ff0a60ce166b44539751e0a51aebcf27037bd
Jaz Ramos <jazminaramos4@hotmail.com>
2023-09-24 17:19:18 -0300

    3

--- js/main.js
+++ js/main.js
@@ -17,21 +17,6 @@ function cristalesCards(lista) {
     }
 }
 
-let botones = document.getElementsByClassName('compra');//coleccion de nodos
-    for (const boton of botones) {
-        //opcion 1 - addEventListener
-        boton.addEventListener('click', () => {
-            console.log('Hiciste click en el boton cuyo id es ' + boton.id);
-            const prodACarro = listaProds.find((producto) => producto.id == boton.id);
-            console.log(prodACarro);
-            //cargar producto encontrado al carro
-            agregarAlCarrito(prodACarro);
-        });
-
-        //opcion 2
-        boton.onmouseover = () => boton.classList.replace('btn-primary', 'btn-warning');
-        boton.onmouseout = () => boton.classList.replace('btn-warning', 'btn-primary');
-    }
 
 
 cristalesCards(productos);
