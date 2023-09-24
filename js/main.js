7fa10a55fa41713646fdaa91a5eb6b50392678e4
Jaz Ramos <jazminaramos4@hotmail.com>
2023-09-24 17:06:42 -0300

    3

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ const cardsProductos = document.getElementById("cristales");
 function cristalesCards(lista) {
     for (const prod of lista) {
         cardsProductos.innerHTML += `
-        <div class="col animate__animated animate__fadeIn ">
-            <div class="card">
+            <div class="card" style="width: 30rem;>
                 <img src="${prod.foto}" class="card-img-top" alt="${prod.nombre}"/>
                 <div class="card-body">
                     <h5 class="card-title">${prod.nombre}</h5>
@@ -14,9 +13,25 @@ function cristalesCards(lista) {
                     <button type=${prod.id} class="btn btn-info">Comprar</button>
                 </div>
             </div>
-        </div>
         `;
     }
 }
+
+let botones = document.getElementsByClassName('compra');//coleccion de nodos
+    for (const boton of botones) {
+        //opcion 1 - addEventListener
+        boton.addEventListener('click', () => {
+            console.log('Hiciste click en el boton cuyo id es ' + boton.id);
+            const prodACarro = listaProds.find((producto) => producto.id == boton.id);
+            console.log(prodACarro);
+            //cargar producto encontrado al carro
+            agregarAlCarrito(prodACarro);
+        });
+
+        //opcion 2
+        boton.onmouseover = () => boton.classList.replace('btn-primary', 'btn-warning');
+        boton.onmouseout = () => boton.classList.replace('btn-warning', 'btn-primary');
+    }
+
 
 cristalesCards(productos);
--- js/productos.js
+++ js/productos.js
@@ -3,28 +3,28 @@ const productos = [{
     foto: "https://cdn.pixabay.com/photo/2018/04/05/08/47/agate-3292211_1280.jpg",
     nombre: "Ágata",
     descripcion: "Transmite energía positiva, vitalidad aumentando el autoestima. Logros materiales.",
-    precio: $1000
+    precio: 1000
 },
 {
     id: 2,
     foto: "https://cdn.pixabay.com/photo/2018/04/17/17/28/amethyst-3328161_1280.jpg",
     nombre: "Amatista",
     descripcion: "Piedra de la transmutación. Calma la mente. Facilita el sueño.",
-    precio: $1000
+    precio: 1000
 },
 {
     id: 3,
     foto: "https://cdn.pixabay.com/photo/2021/03/01/04/38/amazonite-6058741_1280.jpg",
     nombre: "Amazonita",
     descripcion: "Facilita la autoexpresión. Alivia la pena emocional. Disipa miedo e inseguridad.",
-    precio: $1000
+    precio: 1000
 },
 {
     id: 4,
     foto: "https://cdn.pixabay.com/photo/2018/03/05/19/49/citrine-3201605_1280.jpg",
     nombre: "Citrino",
     descripcion: "Autoestima. Poder personal. Planes y objetivos. Confianza en uno mismo.",
-    precio: $1000
+    precio: 1000
 }
 
 ];
